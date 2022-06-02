c8042be32217dfa2a65ec770b1f7bd4529bea897
kmaasrud <km@aasrud.com>
2022-06-02 16:37:34 +0200

    Simplify Command::from with slice patterns

--- src/command.rs
+++ src/command.rs
@@ -11,33 +11,15 @@ pub struct Command {
 
 impl Command {
   pub(crate) fn from(command: Vec<String>) -> Option<Self> {
-    let (prefix, command) = command.split_at(1);
-
-    if let Some(prefix) = prefix.get(0) {
-      if prefix != PREFIX {
-        return None;
-      }
+    match &*command {
+      [prefix, program, arguments @ ..] if prefix == PREFIX => Some(Self {
+        program: program.to_string(),
+        arguments: arguments.to_owned(),
+      }),
+      _ => None,
     }
-
-    if command.is_empty() {
-      return None;
-    }
-
-    let command = command
-      .join(" ")
-      .split(' ')
-      .map(|s| s.into())
-      .collect::<Vec<String>>();
-
-    let (program, arguments) = command.split_at(1);
-
-    program.first().map(|program| Self {
-      program: program.to_string(),
-      arguments: arguments.to_owned(),
-    })
   }
 
-  // TODO: Command should only get command output, not handle diffs
   pub(crate) fn execute(&self) -> Result<String> {
     let output = process::Command::new(self.program.clone())
       .args(self.arguments.clone())
